75f5e92a7b3ea003cdead1ffa3e3c9c44933ac56
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-20 14:01:25 +0000

    pushed it to the repository

---
+++ script.js
@@ -0,0 +1,58 @@
+let doors = [];
+const doorBox = document.querySelectorAll(".door-box");
+const randomNumber = Math.floor(Math.random()*doors.length);
+let playGame = true;
+const ansYes = document.querySelector(".yes");
+const ansNo = document.querySelector(".no");
+const answer = document.querySelector(".answer");
+const title = document.querySelector(".title");
+
+// get all the doors id from the div tags and store it in the doors array
+doorBox.forEach(element=>{
+    doors.push(document.getElementById(element.firstElementChild.id))
+})
+
+
+// loop through the doors and add eventlistener to it when the user clicks on it
+doors.forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+    title.innerHTML = "";
+    for(let i=0; i<doors.length; i++){
+        if(i===randomNumber){
+             doorBox[i].children[1].innerHTML = `<img src="car.png" />`;
+             
+        }
+        else{
+             doorBox[i].children[1].innerHTML = `<img src="goat.png" />`;
+        }
+    }
+     let doorSelected = e.path[1].id; 
+     let box =  document.querySelector(`.${e.path[1].id}-box`);
+     box.children[1].style.display = "block";
+     let imageSrc = box.children[1].firstChild.src.split('/')
+     let imageData = imageSrc[imageSrc.length-1].split(".")[0];
+    if(imageData==="car"){
+        title.innerHTML = "You win 🏆";
+    }
+    else{
+       title.innerHTML = "You lose 😞";
+        answer.style.display = "block";
+        [ansYes, ansNo].forEach(element=>{
+            element.addEventListener("click",(e)=>{
+                if(e.path[0].innerHTML==="yes"){
+                    box.parentNode.removeChild(document.querySelector(`.${ doorSelected }-box`));
+                    answer.style.display = "none";
+                      title.innerHTML = "";
+                }
+           else{
+               title.innerHTML = "Game Over 🚫";
+               answer.style.display = "none";
+           }
+            });
+        });
+    }
+       
+   });
+    
+    
+});
