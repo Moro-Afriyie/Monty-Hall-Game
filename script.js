acbe5e25e9a7e0ad148fbe01b0c0211bc81c1d63
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-20 21:48:27 +0000

    fixed the multiple event listeners bug

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const title = document.querySelector(".title");
 
 // get all the doors id from the div tags and store it in the doors array
 doorBox.forEach(element=>{
-    doors.push(document.getElementById(element.firstElementChild.id))
+    doors.push(document.getElementById(element.firstElementChild.id));
 });
 
 const randomNumber = Math.floor(Math.random()*doorBox.length);
@@ -16,6 +16,11 @@ const randomNumber = Math.floor(Math.random()*doorBox.length);
 const handleGamePlay = (e)=>{
     e.path[0].style.transform = "rotateY(180deg)";
     title.innerHTML = "";
+    doors.forEach((door)=>{
+        if(door !== e.currentTarget){
+        door.removeEventListener('click', handleGamePlay);
+                }
+        });
     for(let i=0; i<doors.length; i++){
         if(i===randomNumber){
              doorBox[i].children[1].innerHTML = `<img src="car.png" />`;
@@ -26,9 +31,9 @@ const handleGamePlay = (e)=>{
     }
      let doorSelected = e.path[1].id;  // gets the id of the door which was selected
      let box =  document.querySelector(`.${e.path[1].id}-box`);
-     box.children[1].style.display = "block"; // get the parent div
      let imageSrc = box.children[1].firstChild.src.split('/') // get the image src
      let imageData = imageSrc[imageSrc.length-1].split(".")[0]; // get the value of the image
+     
     if(imageData==="car"){
         title.innerHTML = "You win 🏆";
     }
@@ -41,6 +46,10 @@ const handleGamePlay = (e)=>{
                     box.parentNode.removeChild(document.querySelector(`.${ doorSelected }-box`));
                     answer.style.display = "none";
                       title.innerHTML = "";
+                      doors.forEach((element)=>{
+    element.addEventListener('click', handleGamePlay);
+
+});
                 }
            else{
                title.innerHTML = "Game Over 🚫";
@@ -55,4 +64,5 @@ const handleGamePlay = (e)=>{
 // loop through the doors and add eventlistener to it when the user clicks on it
 doors.forEach((element)=>{
     element.addEventListener('click', handleGamePlay);
+
 });
